1947aa9809f2b77f992f6b7c2aa21fa09e6ce912
Chandler Peck <48533868+Rowley-dev@users.noreply.github.com>
2025-06-05 20:42:51 -0500

    Update AmuletOfGlory.kt

--- content/items/jewellery/src/main/kotlin/org/rsmod/content/items/jewellery/amulet_of_glory/scripts/AmuletOfGlory.kt
+++ content/items/jewellery/src/main/kotlin/org/rsmod/content/items/jewellery/amulet_of_glory/scripts/AmuletOfGlory.kt
@@ -9,18 +9,8 @@ import org.rsmod.api.player.output.ClientScripts.chatboxMultiInit
 import org.rsmod.api.player.output.ClientScripts.topLevelChatboxResetBackground
 import org.rsmod.api.player.protect.ProtectedAccess
 import org.rsmod.api.repo.world.WorldRepository
-import org.rsmod.api.script.onOpHeld1
-import org.rsmod.api.script.onOpHeld2
-import org.rsmod.api.script.onOpHeld3
-import org.rsmod.api.script.onOpHeld4
-import org.rsmod.api.script.onOpObj3
-import org.rsmod.api.script.onPlayerQueue
-import org.rsmod.api.script.onPlayerQueueWithArgs
-import org.rsmod.content.items.jewellery.amulet_of_glory.configs.AMULET_OF_GLORY_LOCATIONS
-import org.rsmod.content.items.jewellery.amulet_of_glory.configs.amulet_of_glory_seqs
-import org.rsmod.content.items.jewellery.amulet_of_glory.configs.amulet_of_glory_spotanims
-import org.rsmod.content.items.jewellery.amulet_of_glory.configs.amulet_of_glory_synths
-import org.rsmod.content.items.jewellery.amulet_of_glory.configs.amulets_of_glory
+import org.rsmod.api.script.*
+import org.rsmod.content.items.jewellery.amulet_of_glory.configs.*
 import org.rsmod.game.inv.InvObj
 import org.rsmod.game.inv.isType
 import org.rsmod.game.type.obj.ObjType
@@ -30,30 +20,31 @@ import org.rsmod.map.CoordGrid
 import org.rsmod.plugin.scripts.PluginScript
 import org.rsmod.plugin.scripts.ScriptContext
 
-class AmuletOfGlory @Inject constructor(private val worldRepo: WorldRepository) : PluginScript() {
+class AmuletOfGlory @Inject constructor(
+    private val worldRepo: WorldRepository
+) : PluginScript() {
+
     override fun ScriptContext.startup() {
         onOpHeld4(content.amulet_of_glory) { gloryDialogue(it.type, it.obj, it.slot) }
         onPlayerQueueWithArgs<Pair<CoordGrid, InvObj>>(queues.jewellery_teleport) {
             val (coords, item) = it.args
             gloryTeleport(coords, item)
         }
-
     }
 
-    private suspend fun ProtectedAccess.gloryDialogue(type: UnpackedObjType, item: InvObj, invSlot: Int) {
+    private suspend fun ProtectedAccess.gloryDialogue(
+        type: UnpackedObjType,
+        item: InvObj,
+        invSlot: Int
+    ) {
         mes("You rub the amulet...")
         topLevelChatboxResetBackground(player)
         val selection = choice5(
-            "Edgeville",
-            1,
-            "Karamja",
-            2,
-            "Draynor Village",
-            3,
-            "Al Kharid",
-            4,
-            "Nowhere",
-            5,
+            "Edgeville", 1,
+            "Karamja", 2,
+            "Draynor Village", 3,
+            "Al Kharid", 4,
+            "Nowhere", 5,
             title = "Where would you like to teleport to?"
         )
         when (selection) {
@@ -65,7 +56,12 @@ class AmuletOfGlory @Inject constructor(private val worldRepo: WorldRepository) 
         }
     }
 
-    private fun ProtectedAccess.gloryPreTeleport(type: UnpackedObjType, item: InvObj, selection: CoordGrid, invSlot: Int) {
+    private fun ProtectedAccess.gloryPreTeleport(
+        type: UnpackedObjType,
+        item: InvObj,
+        selection: CoordGrid,
+        invSlot: Int
+    ) {
         val replacement = type.param(params.replacement_item_id)
 
         anim(amulet_of_glory_seqs.amulet_of_glory_teleport)
@@ -91,14 +87,30 @@ class AmuletOfGlory @Inject constructor(private val worldRepo: WorldRepository) 
 
     private fun ProtectedAccess.getMessage(item: InvObj): String? =
         when {
-            item.isType(amulets_of_glory.amulet_of_glory_6) || item.isType(amulets_of_glory.trail_amulet_of_glory_6) -> "<col=7f007f>Your amulet has five charges left.</col>"
-            item.isType(amulets_of_glory.amulet_of_glory_5) || item.isType(amulets_of_glory.trail_amulet_of_glory_5) -> "<col=7f007f>Your amulet has four charges left.</col>"
-            item.isType(amulets_of_glory.amulet_of_glory_4) || item.isType(amulets_of_glory.trail_amulet_of_glory_4) -> "<col=7f007f>Your amulet has three charges left.</col>"
-            item.isType(amulets_of_glory.amulet_of_glory_3) || item.isType(amulets_of_glory.trail_amulet_of_glory_3) -> "<col=7f007f>Your amulet has two charges left.</col>"
-            item.isType(amulets_of_glory.amulet_of_glory_2) || item.isType(amulets_of_glory.trail_amulet_of_glory_2) -> "<col=7f007f>Your amulet has one charge left.</col>"
-            item.isType(amulets_of_glory.amulet_of_glory_1) || item.isType(amulets_of_glory.trail_amulet_of_glory_1) -> "<col=7f007f>Your amulet has run out of charges.</col>"
+            item.isType(amulets_of_glory.amulet_of_glory_6) ||
+                item.isType(amulets_of_glory.trail_amulet_of_glory_6) ->
+                "<col=7f007f>Your amulet has five charges left.</col>"
+
+            item.isType(amulets_of_glory.amulet_of_glory_5) ||
+                item.isType(amulets_of_glory.trail_amulet_of_glory_5) ->
+                "<col=7f007f>Your amulet has four charges left.</col>"
+
+            item.isType(amulets_of_glory.amulet_of_glory_4) ||
+                item.isType(amulets_of_glory.trail_amulet_of_glory_4) ->
+                "<col=7f007f>Your amulet has three charges left.</col>"
+
+            item.isType(amulets_of_glory.amulet_of_glory_3) ||
+                item.isType(amulets_of_glory.trail_amulet_of_glory_3) ->
+                "<col=7f007f>Your amulet has two charges left.</col>"
+
+            item.isType(amulets_of_glory.amulet_of_glory_2) ||
+                item.isType(amulets_of_glory.trail_amulet_of_glory_2) ->
+                "<col=7f007f>Your amulet has one charge left.</col>"
+
+            item.isType(amulets_of_glory.amulet_of_glory_1) ||
+                item.isType(amulets_of_glory.trail_amulet_of_glory_1) ->
+                "<col=7f007f>Your amulet has run out of charges.</col>"
 
             else -> null
         }
-    }
-
+}
